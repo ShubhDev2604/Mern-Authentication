c23d89cbfb32b5f9b0c8686c06e6db44985ae2f7
ShubhDev2604 <shubhtandon802@gmail.com>
2023-12-31 19:44:56 +0530

    Added Comments

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
-
+{
+  /*div tag is used to make different differnt sections in webpage
+className is used to apply css attributes to material inside the tag.*/
+}
 export default function Header() {
   return (
     <div className="bg-slate-200">
+      {" "}
+      {/* Makes the division slate color */}
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        {/*justify set the flex items horizontally ,
+      items-center push Auth App to center of header vertically, max-w-6xl sets width between screen and items, max-auto does,
+  that to both sides, and p-3 applies padding of 3 size to words.*/}
         <Link to="/">
+          {/*Link to add Link to the text in the tag*/}
           <h1 className="font-bold">Auth App</h1>
         </Link>
         <ul className="flex gap-4">
+          {/*Ul represents unordered list while li represents an item of that unordered list*/}
           <Link to="/">
             <li>Home</li>
           </Link>
